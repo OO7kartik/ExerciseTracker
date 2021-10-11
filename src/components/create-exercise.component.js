0fbce217f91eb322a7c3f307d135ffe4451f5a56
OO7kartik <rohiit.audrey@gmail.com>
2021-10-12 00:06:43 +0530

    Add create-exercise to db

--- src/components/create-exercise.component.js
+++ src/components/create-exercise.component.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DatePicker from 'react-datepicker';
+import axios from 'axios'
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class CreateExercise extends Component {
@@ -23,10 +24,15 @@ export default class CreateExercise extends Component {
   }
 
   componentDidMount() {
-    this.setState({
-      users: ['test1'],
-      username: 'test1'
-    })
+    axios.get('http://localhost:5000/users/')
+      .then(response => {
+        if(response.data.length > 0) {
+          this.setState({
+            users: response.data.map(user => user.username),
+            username: response.data[0].username
+          })
+        }
+      })
   }
   
   onChangeUsername(e) {
@@ -65,6 +71,10 @@ export default class CreateExercise extends Component {
     }
 
     console.log(exercise);
+
+    
+    axios.post('http://localhost:5000/exercises/add', exercise) 
+      .then(res => console.log(res.data));
 
     window.location = '/';
   }
