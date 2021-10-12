65f7fd8abafc44e1a96af066d17294876cece2bd
OO7kartik <rohiit.audrey@gmail.com>
2021-10-12 22:11:14 +0530

    Fix edit-exercise bug

--- src/components/edit-exercise.component.js
+++ src/components/edit-exercise.component.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import axios from 'axios';
 import DatePicker from 'react-datepicker';
-import axios from 'axios'
 import "react-datepicker/dist/react-datepicker.css";
 
 export default class EditExercise extends Component {
@@ -12,7 +12,6 @@ export default class EditExercise extends Component {
     this.onChangeDuration = this.onChangeDuration.bind(this);
     this.onChangeDate = this.onChangeDate.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
-
 
     this.state = {
       username: '',
@@ -30,48 +29,50 @@ export default class EditExercise extends Component {
           username: response.data.username,
           description: response.data.description,
           duration: response.data.duration,
-          data: new Date(response.data.date)
-        })
+          date: new Date(response.data.date)
+        })   
       })
-      .catch((error) => {
+      .catch(function (error) {
         console.log(error);
       })
 
-
     axios.get('http://localhost:5000/users/')
       .then(response => {
-        if(response.data.length > 0) {
+        if (response.data.length > 0) {
           this.setState({
             users: response.data.map(user => user.username),
           })
         }
       })
+      .catch((error) => {
+        console.log(error);
+      })
+
   }
-  
+
   onChangeUsername(e) {
     this.setState({
       username: e.target.value
-    });
+    })
   }
 
   onChangeDescription(e) {
     this.setState({
       description: e.target.value
-    });
+    })
   }
 
   onChangeDuration(e) {
     this.setState({
       duration: e.target.value
-    });
+    })
   }
 
   onChangeDate(date) {
     this.setState({
       date: date
-    });
+    })
   }
-
 
   onSubmit(e) {
     e.preventDefault();
@@ -85,17 +86,15 @@ export default class EditExercise extends Component {
 
     console.log(exercise);
 
-    
-    axios.post('http://localhost:5000/exercises/update'+this.props.match.params.id, exercise) 
+    axios.post('http://localhost:5000/exercises/update/' + this.props.match.params.id, exercise)
       .then(res => console.log(res.data));
 
     window.location = '/';
   }
 
-
   render() {
     return (
-      <div>
+    <div>
       <h3>Edit Exercise Log</h3>
       <form onSubmit={this.onSubmit}>
         <div className="form-group"> 
